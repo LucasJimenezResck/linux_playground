15eb8bc48a28787d29128c71e8ce33e758cdbad3
LucasJimenezResck <fontelaspilas@gmail.com>
2025-10-23 13:39:13 +0200

    changed makefile and added new use case

---
+++ mmap_miniproject/02_reading_files.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <fcntl.h> //open
+#include <unistd.h> //close
+
+
+int main(int argc, char* argv[])
+{
+    printf("Hello Viki!\n");
+    
+    return 0;
+}
